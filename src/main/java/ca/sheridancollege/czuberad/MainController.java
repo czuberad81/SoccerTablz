b6c406d950bb8e1c3e5489718822e87110216698
Adam Czubernat <czuberad@sheridancollege.ca>
2021-02-22 22:36:38 -0500

    Basic form to get user inputs for adding team

--- src/main/java/ca/sheridancollege/czuberad/MainController.java
+++ src/main/java/ca/sheridancollege/czuberad/MainController.java
@@ -1,10 +1,18 @@
 package ca.sheridancollege.czuberad;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    DatabaseAccess da;
 
     @GetMapping("/")
     public String home(){
@@ -12,7 +20,8 @@ public class MainController {
     }
 
     @GetMapping("/addTeam")
-    public String add(){
+    public String add(Model model){
+        model.addAttribute("team", new Team());
         return "add";
     }
     @GetMapping("/editTeam")
@@ -31,5 +40,14 @@ public class MainController {
     public String backHome(){
         return "home";
     }
+
+    @PostMapping("/insertTeam")
+    public String insertTeam(@ModelAttribute Team team){
+        da.insertTeam(team.getTeamName(),team.getContinent(),team.getPlayed(),team.getWon(),team.getDrawn(),team.getLost());
+        return "add";
+
+    }
+
+
 
 }
